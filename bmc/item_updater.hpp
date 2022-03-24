d912a1233f22792d3407d8c254dc0d887292c16d
Adriana Kobylak <anoo@us.ibm.com>
2022-03-24 21:04:51 +0000

    uak: Add sync method
    Upon start, the software updater service reads the User Access Key
    date from the com.ibm.ipzvpd.UTIL D8 property on the bmc card and from
    flash memory in /var/lib/phosphor-bmc-code-mgmt/uak-data.
    Then it syncs them up with one another, prioritizing first the bmc card,
    then flash memory.
    
    Tested: Logged in to the phyp console and updated the UAK date. Verified
    that this registered in the D8 property immediately and that it was
    written to flash memory upon reboot. Verified that syncing happens in
    order of priority. Verified no syncing is done when the value is null,
    0s, or spaces.
    
    Change-Id: I6052f370d2168c45cf4db8d7809abeaa8035d650
    Signed-off-by: Isaac Kurth <blisaac91@gmail.com>
    Signed-off-by: Adriana Kobylak <anoo@us.ibm.com>

--- bmc/item_updater.hpp
+++ bmc/item_updater.hpp
@@ -17,6 +17,10 @@
 #include <string>
 #include <vector>
 
+#ifdef WANT_ACCESS_KEY_VERIFY
+#include "uak_verify.hpp"
+#endif
+
 namespace phosphor
 {
 namespace software
@@ -103,6 +107,11 @@ class ItemUpdater : public ItemUpdaterInherit
                 std::bind(std::mem_fn(&ItemUpdater::createActivation), this,
                           std::placeholders::_1));
         }
+#ifdef WANT_ACCESS_KEY_VERIFY
+        using UpdateAccessKey = phosphor::software::image::UpdateAccessKey;
+        UpdateAccessKey updateAccessKey("");
+        updateAccessKey.sync();
+#endif
         getRunningSlot();
         setBMCInventoryPath();
         if (type == UpdaterType::BMC || type == UpdaterType::ALL)
