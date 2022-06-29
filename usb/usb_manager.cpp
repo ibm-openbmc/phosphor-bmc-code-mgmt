c7ea0c889b0e07707a7c9b05fc9d51b944d323f4
Zami Seck <Aminata.Seck@ibm.com>
2022-06-29 16:42:35 -0400

    Add Progress Code For USB Code Update (#31)
    This change ensures that update progress codes are shown on
    the op panel during the process of a firmware change. There
    are four update codes which indicate that the firmware is:
    1. C100B101: The firmware image is being installed on a side of
    the flash.
    2. C100B102: The firmware image is being installed on the other
    side of the flash.
    3. C100B103: The firmware installation has been completed
    successfully.
    4. C100B104: code is displayed, the firmware installation has
    failed.
    This reassures the user that the code update process is happening
    and they will not need access to the GUI for further information.
    
    Tested:
    Made sure that the appropriate code was displayed on the op panel
    and log within the progress code logs during code update.
    
    USB Code Update: Cleanup (#39)
    
    * USB Code Update Cleanup: Fail Code
    
    These changes ensure that the fail code that should be displayed
    when a USB code update attempt is only displayed then. Previously,
    the C100B104 fail could be seen on the op panel during other types
    of updates as well as USB code update. These changes ensure that
    it is only visible when appropriate.
    
    * USB Code Update Cleanup: Clearing Directory
    
    When USB code update fails and is later retried, the update begins
    updating the second side rather than starting the code update
    from the beginning. These changes address that issue by clearing
    directories after failure so upon reattempt, the USB code update works
    as expected.
    
    * USB Code Update Cleanup
    
    Because the fail progress code is no longer being called from the
    activation.cpp file, there is no longer a need for the progress
    code logic to be in utils. These changes move the progress code calls
    from the utils files to the USB manager files.
    
    Change-Id: I46572f061b6a9d02ad2acea97cd212b61dcdd3b4
    Signed-off-by: zamiseck <zimzam17@gmail.com>

--- usb/usb_manager.cpp
+++ usb/usb_manager.cpp
@@ -41,6 +41,7 @@ bool USBManager::run()
 
             if (!setUSBProgress())
             {
+                writeSuccess(bus);
                 lg2::info("USB Code Update: Completed updating both sides");
                 break;
             }
@@ -70,6 +71,7 @@ bool USBManager::setUSBProgress()
     if (!fs::exists(progress1))
     {
         std::ofstream os(progress1.c_str());
+        writeSideOneProgress(bus);
         return true;
     }
 
@@ -77,6 +79,7 @@ bool USBManager::setUSBProgress()
     if (!fs::exists(progress2))
     {
         std::ofstream os(progress2.c_str());
+        writeSideTwoProgress(bus);
         return true;
     }
 
@@ -146,8 +149,8 @@ void USBManager::updateActivation(sdbusplus::message_t& msg)
             if (imageProp == readyPro && isUSBCodeUpdate)
             {
                 setApplyTime();
+                listenActivationState(path.str);
                 setRequestedActivation(path.str);
-                event.exit(0);
             }
         }
         catch (const std::exception& e)
@@ -156,6 +159,179 @@ void USBManager::updateActivation(sdbusplus::message_t& msg)
                        "ERROR", e.what());
         }
     }
+}
+
+void USBManager::bmcCheckState(sdbusplus::message::message& msg)
+{
+    std::string interface, bmcState;
+    std::map<std::string, std::variant<std::string>> properties;
+
+    try
+    {
+        msg.read(interface, properties);
+
+        for (const auto& p : properties)
+        {
+            if (p.first == "CurrentBMCState")
+            {
+                bmcState = std::get<std::string>(p.second);
+                break;
+            }
+        }
+
+        if (bmcState.empty())
+        {
+            return;
+        }
+        else if (bmcState == "xyz.openbmc_project.State.BMC.BMCState.Ready")
+        {
+            if (!run())
+            {
+                event.exit(0);
+            }
+            isUSBCodeUpdate = true;
+        }
+    }
+    catch (const std::exception& e)
+    {
+        lg2::error("Failed in getting CurrentBMCState, ERROR:{ERROR}", "ERROR",
+                   e.what());
+    }
+}
+
+void USBManager::listenActivationState(const std::string& path)
+{
+    static std::unique_ptr<sdbusplus::bus::match::match> activationState =
+        std::make_unique<sdbusplus::bus::match::match>(
+            bus,
+            MatchRules::propertiesChanged(
+                path, "xyz.openbmc_project.Software.Activation"),
+            [this](sdbusplus::message::message& msg) {
+        activationChangeEvent(msg);
+    });
+}
+
+void USBManager::activationChangeEvent(sdbusplus::message::message& msg)
+{
+    std::string interface, activationStatus;
+    std::map<std::string, std::variant<std::string>> properties;
+
+    constexpr auto failedStatus =
+        "xyz.openbmc_project.Software.Activation.Activations.Failed";
+    constexpr auto activeStatus =
+        "xyz.openbmc_project.Software.Activation.Activations.Active";
+    auto usbDir = fs::path(PERSIST_DIR) / "usb";
+
+    try
+    {
+        msg.read(interface, properties);
+
+        for (const auto& p : properties)
+        {
+            if (p.first == "Activation")
+            {
+                activationStatus = std::get<std::string>(p.second);
+                break;
+            }
+        }
+
+        if (activationStatus.empty())
+        {
+            lg2::error("activationStatus was either empty or no match");
+            return;
+        }
+        else if (activationStatus == failedStatus)
+        {
+            lg2::error("activationStatus found. Failed status.");
+            fs::remove_all(usbDir);
+            writeFail(bus);
+            event.exit(0);
+        }
+        else if (activationStatus == activeStatus)
+        {
+            event.exit(0);
+        }
+    }
+    catch (const std::exception& e)
+    {
+        lg2::error("Failed in getting Activation, ERROR:{ERROR}", "ERROR",
+                   e.what());
+    }
+}
+
+void USBManager::setProgressSRC(
+    const std::tuple<uint64_t, std::vector<uint8_t>>& prgCode,
+    sdbusplus::bus::bus& bus)
+{
+    static constexpr auto bootRawProgress =
+        "xyz.openbmc_project.State.Boot.Raw";
+    static constexpr auto bootRawSetting =
+        "/xyz/openbmc_project/state/boot/raw0";
+    static constexpr auto dbusProperty = "org.freedesktop.DBus.Properties";
+    static constexpr auto rawProperty = "Value";
+    static constexpr auto setMethod = "Set";
+
+    try
+    {
+        auto method = bus.new_method_call(bootRawProgress, bootRawSetting,
+                                          dbusProperty, setMethod);
+        method.append(
+            bootRawProgress, rawProperty,
+            std::variant<std::tuple<uint64_t, std::vector<uint8_t>>>(prgCode));
+        bus.call(method);
+    }
+    catch (std::exception& e)
+    {
+        lg2::error(
+            "Failed to call d-bus call to boot progress interface:{ERROR}",
+            "ERROR", e);
+    }
+}
+
+void USBManager::writeProgressSRC(const std::vector<uint8_t>& primArray,
+                                  sdbusplus::bus::bus& bus)
+{
+    uint64_t primaryCode = 0;
+    // Initialize vector that acts as the secondary progress code which contains
+    // the ascii (8-bit) primary code which is displayed on the op panel and the
+    // secondary code which contain hex words.
+    std::vector<uint8_t> secArr{
+        2,  0,  0,  1,  0,  0,  0,  72, 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+        0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+        0,  0,  0,  32, 0,  32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32,
+        32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32};
+
+    for (int i = 0; i < 8; i++)
+    {
+        secArr[40 + i] = secArr[i];
+        primaryCode |= (uint64_t)primArray[i] << 8 * i;
+    }
+
+    setProgressSRC(std::make_tuple(primaryCode, secArr), bus);
+}
+
+void USBManager::writeSuccess(sdbusplus::bus::bus& bus)
+{
+    std::vector<uint8_t> successArray{67, 49, 48, 48, 66, 49, 48, 51};
+    writeProgressSRC(successArray, bus);
+}
+
+void USBManager::writeSideOneProgress(sdbusplus::bus::bus& bus)
+{
+    std::vector<uint8_t> sameSideArray{67, 49, 48, 48, 66, 49, 48, 49};
+    writeProgressSRC(sameSideArray, bus);
+}
+
+void USBManager::writeSideTwoProgress(sdbusplus::bus::bus& bus)
+{
+    std::vector<uint8_t> oppSideArray{67, 49, 48, 48, 66, 49, 48, 50};
+    writeProgressSRC(oppSideArray, bus);
+}
+
+void USBManager::writeFail(sdbusplus::bus::bus& bus)
+{
+    std::vector<uint8_t> failArray{67, 49, 48, 48, 66, 49, 48, 52};
+    writeProgressSRC(failArray, bus);
 }
 
 } // namespace usb
