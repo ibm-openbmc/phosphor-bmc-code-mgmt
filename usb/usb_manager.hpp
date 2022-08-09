ced2a7b5ad50858fa5575566c087ac98d558c2e9
Zami Seck <Aminata.Seck@ibm.com>
2022-08-09 14:57:21 -0400

    USB Code Update: Cleanup (#39)
    * USB Code Update Cleanup: Fail Code
    
    These changes ensure that the fail code that should be displayed
    when a USB code update attempt is only displayed then. Previously,
    the C100B104 fail could be seen on the op panel during other types
    of updates as well as USB code update. These changes ensure that
    it is only visible when appropriate.
    
    SW554498
    
    * USB Code Update Cleanup: Clearing Directory
    
    When USB code update fails and is later retried, the update begins
    updating the second side rather than starting the code update
    from the beginning. These changes address that issue by clearing
    directories after failure so upon reattempt, the USB code update works
    as expected.
    
    SW554498
    
    * USB Code Update Cleanup
    
    Because the fail progress code is no longer being called from the
    activation.cpp file, there is no longer a need for the progress
    code logic to be in utils. These changes move the progress code calls
    from the utils files to the USB manager files.
    
    SW554498
    
    Co-authored-by: zamiseck <zimzam17@gmail.com>

--- usb/usb_manager.hpp
+++ usb/usb_manager.hpp
@@ -105,6 +105,73 @@ class USBManager
      */
     void bmcCheckState(sdbusplus::message::message& msg);
 
+    /** @brief Api to register activation state callback. This api will register
+     *         callback to listen for activation state property change.
+     *  @param[in] path - activation path
+     */
+    void listenActivationState(const std::string& path);
+
+    /** @brief Displays failed progress code upon update failure.
+     *
+     * @param[in]  msg  - Data associated with subscribed signal
+     */
+    void activationChangeEvent(sdbusplus::message::message& msg);
+
+    /**
+     * @brief method to set the dbus Raw value Property with the obtained
+     *        progress code from code update.
+     *
+     * @param[in] prgCode - the tuple containing primary and secondary progress
+     *            code
+     **/
+    void setProgressSRC(
+        const std::tuple<uint64_t, std::vector<uint8_t>>& prgCode,
+        sdbusplus::bus::bus& bus);
+
+    /**
+     * @brief method to create tuple for dbus method that displays information
+     *        to the op panel and adds an entry to the progress log
+     *
+     * @param[in] primArray - the progress Code SRC
+     **/
+    void writeProgressSRC(const std::vector<uint8_t>& primArray,
+                          sdbusplus::bus::bus& bus);
+
+    /**
+     * @brief method to display success message on op panel during usb code
+     *        update C100B103: The firmware installation has been completed
+     *        successfully.
+     *
+     * @param[in] bus
+     **/
+    void writeSuccess(sdbusplus::bus::bus& bus);
+
+    /**
+     * @brief method to display same side installation message on op panel
+     *        during usb code update C100B101: The firmware image is being
+     *        installed on a side of the flash.
+     *
+     * @param[in] bus
+     **/
+    void writeSideOneProgress(sdbusplus::bus::bus& bus);
+
+    /**
+     * @brief method to display opposite side installation message on op panel
+     *        during usb code update C100B102: The firmware image is being
+     *        installed on the other side of the flash.
+     *
+     * @param[in] bus
+     **/
+    void writeSideTwoProgress(sdbusplus::bus::bus& bus);
+
+    /**
+     * @brief method to display fail message on op panel during usb code update
+     *        C100B104 code is displayed, the firmware installation has failed.
+     *
+     * @param[in] bus
+     **/
+    void writeFail(sdbusplus::bus::bus& bus);
+
   private:
     /** @brief Persistent sdbusplus DBus bus connection. */
     sdbusplus::bus_t& bus;
