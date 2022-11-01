9d258b8e3a72d69de29cc66c9ac9119f40374c34
Adriana Kobylak <anoo@us.ibm.com>
2022-11-01 08:31:51 -0500

    msl: Store Min Ship Level in VPD (#47)
    Store the minimum ship level in VPD so that only systems shipped from
    manufacturing have this enabled, otherwise if the minimum ship level is
    added as a compile-time option, all systems in the field would have this
    enabled when they upgrade to the image that has the flag.
    
    Provide a tool to set the VPD value to 1030 for manufacturing to use.
    Also set this value when fieldmode is enabled to set the VPD value
    automatically until manufacturing updates their process to add the new
    tool.
    
    Provide a bypass option for PE that sets the VPD value to GA 1020 to
    allow downgrades in the field.
    
    Tested:
    - Enabling fieldmode and also calling "software-manager-tool
      --setminlevel" set the VPD to 1030:
    root@p10bmc:~# vpd-tool -r -O /system/chassis/motherboard -R VSYS -K FV
    {
        "/system/chassis/motherboard": {
            "FV": "fw1030.00-00                    "
        }
    }
    
    - Downgrades to 1020 are not allowed:
    Nov 01 00:49:20 p10bmc phosphor-image-updater[2623]: BMC Minimum Ship
    Level (fw1030.00-00                    ) NOT met by
    fw1020.20-23-1020.2244.20221028a (NL1020_088)
    
    - The bypass sets the VPD to 1020 and downgrades are then allowed:
    root@p10bmc:~# software-manager-tool --resetminlevel
    root@p10bmc:~# vpd-tool -r -O /system/chassis/motherboard -R VSYS -K FV
    {
        "/system/chassis/motherboard": {
            "FV": "fw1020.00-00                    "
        }
    }
    
    Change-Id: I3445cdbfb693595d640226587935cf1d77cefd8c
    Signed-off-by: Adriana Kobylak <anoo@us.ibm.com>

--- bmc/software_manager_tool.cpp
+++ bmc/software_manager_tool.cpp
@@ -1,4 +1,5 @@
 #include "code_updater_manager.hpp"
+#include "msl_verify.hpp"
 
 #include <CLI/CLI.hpp>
 #include <phosphor-logging/lg2.hpp>
@@ -8,15 +9,36 @@ int main(int argc, char** argv)
 {
     namespace fs = std::filesystem;
 
-    CLI::App app{"OpenBMC software manager tool"};
+    bool setMinLevel = false;
+    bool resetMinLevel = false;
 
     std::string imagePath{};
 
+    CLI::App app{"OpenBMC software manager tool"};
+
+    app.add_flag(
+        "--setminlevel", setMinLevel,
+        "Set the minimum ship level to the running version of the system");
+    app.add_flag("--resetminlevel", resetMinLevel,
+                 "Reset the minimum ship level to allow a firmware dowgrade");
     app.add_option(
         "--codeupdate", imagePath,
         "Perform code update with new image on specified path");
 
     CLI11_PARSE(app, argc, argv);
+
+    if (setMinLevel)
+    {
+        minimum_ship_level::set();
+    }
+    else if (resetMinLevel)
+    {
+        minimum_ship_level::reset();
+    }
+    else
+    {
+        std::cout << app.help("", CLI::AppFormatMode::All) << std::endl;
+    }
 
     if (imagePath.empty())
     {
