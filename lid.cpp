7b66f6bbdaf6fb99c7d2676d3893da2484f35e22
Archana Kakani <archana.kakani@ibm.com>
2023-05-05 00:31:01 -0500

    Assemble lids to create a code update image tar ball
    The firmware image for an inband code update is made up of
    individual binary files called LID files. As per current design,
    PLDM reassembles the individual LID files into a tarball image
    that can then be copied to the /tmp/images/ directory to trigger
    the same code update path as the Redfish API.
    
    Moving the Assemble lids part to phosphor-bmc-code-mgmt repo
    where the phosphor-bmc-code-mgmt code executes the shell script to
    assemble the lids and provide the result through activation
    interface (active/invalid) under the version path.
    
    Tested:
        Inband code update success and fail path.
    
    busctl tree xyz.openbmc_project.Software.BMC.Updater
    `-/xyz
      `-/xyz/openbmc_project
        `-/xyz/openbmc_project/software
          |-/xyz/openbmc_project/software/ac4336da
          `-/xyz/openbmc_project/software/e1c899ef
    
    In failure Case:
    Activation:
        busctl get-property xyz.openbmc_project.Software.BMC.Updater
           /xyz/openbmc_project/software/ac4336da
           xyz.openbmc_project.Software.Activation Activation
        --> s "xyz.openbmc_project.Software.Activation.Activations.Invalid"
    
    Version:
        busctl get-property xyz.openbmc_project.Software.BMC.Updater
           /xyz/openbmc_project/software/ac4336da
           xyz.openbmc_project.Software.Version Version
        --> s "null"
    
    In Success case:
    Activation:
        busctl get-property xyz.openbmc_project.Software.BMC.Updater
           /xyz/openbmc_project/software/e1c899ef
           xyz.openbmc_project.Software.Activation Activation
        --> s "xyz.openbmc_project.Software.Activation.Activations.Active"
    
    Version:
        busctl get-property xyz.openbmc_project.Software.BMC.Updater
           /xyz/openbmc_project/software/e1c899ef
           xyz.openbmc_project.Software.Version Version
        --> s "fw1030.20-19-1030.2315.20230404a (NL1030_055)"
    
    Signed-off-by: Archana Kakani <archana.kakani@ibm.com>

--- lid.cpp
+++ lid.cpp
@@ -2,6 +2,9 @@
 
 #include "lid.hpp"
 
+#include "utils.hpp"
+#include "version.hpp"
+
 #include <elog-errors.hpp>
 #include <phosphor-logging/elog.hpp>
 #include <phosphor-logging/lg2.hpp>
@@ -18,6 +21,8 @@ namespace software
 {
 namespace manager
 {
+namespace server = sdbusplus::xyz::openbmc_project::Software::server;
+using VersionClass = phosphor::software::manager::Version;
 PHOSPHOR_LOG2_USING;
 
 void Lid::validate(std::string filePath)
@@ -28,6 +33,80 @@ void Lid::validate(std::string filePath)
 
 #endif
     return;
+}
+
+void Lid::assembleCodeUpdateImage()
+{
+    info("InbandCodeUpdate: assembleCodeUpdateImage\n");
+    utils::subscribeToSystemdSignals(bus);
+    auto method = bus.new_method_call(SYSTEMD_BUSNAME, SYSTEMD_PATH,
+                                      SYSTEMD_INTERFACE, "StartUnit");
+    method.append("assemble-lids.service", "replace");
+    bus.call_noreply(method);
+}
+
+void Lid::unitStateChange(sdbusplus::message_t& msg)
+{
+    uint32_t newStateID{};
+    sdbusplus::message::object_path newStateObjPath;
+    std::string newStateUnit{};
+    std::string newStateResult{};
+
+    // Read the msg and populate each variable
+    msg.read(newStateID, newStateObjPath, newStateUnit, newStateResult);
+
+    auto assembleLidServiceFile = "assemble-lids.service";
+
+    if (newStateUnit == assembleLidServiceFile)
+    {
+        info(
+            "InbandCodeUpdate: Service  - {SERVICEFILE} and Service Return value - {SERVICERESULT}",
+            "SERVICEFILE", newStateUnit, "SERVICERESULT", newStateResult);
+        // unsubscribe to systemd signals
+        utils::unsubscribeFromSystemdSignals(bus);
+
+        if (newStateResult == "done")
+        {
+            info(
+                "InbandCodeUpdate: Assemble Code update image is completed successfully.");
+        }
+        else
+        {
+            // Create the empty version object path
+            error("InbandCodeUpdate: Assemble Code update image Failed");
+            createVersionInterface();
+        }
+    }
+}
+
+void Lid::createVersionInterface()
+{
+    // Creating a Version interface under Updater service to provide the failed
+    // result of assemble code update image task. This result is read by PLDM
+    // service
+    auto versionId = VersionClass::getId(std::to_string(randomGen()));
+
+    auto path = std::string{SOFTWARE_OBJPATH} + '/' + versionId;
+    info("InbandCodeUpdate - Created Version interface {VERSION}", "VERSION",
+         path);
+
+    auto version = "null";
+
+    auto dummyErase = [](std::string /*entryId*/) {
+        // Do nothing;
+    };
+
+    // Staging directory
+    auto imageDir = "/var/lib/phosphor-software-manager/hostfw/staging";
+
+    auto assembleLidVersion = std::make_unique<VersionClass>(
+        bus, path, version, server::Version::VersionPurpose::BMC, "", imageDir,
+        std::vector<std::string>(),
+        std::bind(dummyErase, std::placeholders::_1), "");
+
+    assembleLidVersion->deleteObject =
+        std::make_unique<phosphor::software::manager::Delete>(
+            bus, path, *assembleLidVersion);
 }
 
 } // namespace manager
