35923aff00c3134cc156861d32579744174d087d
ArchanaKakani <101324017+ArchanaKakani@users.noreply.github.com>
2023-05-09 00:56:50 +0530

    Assemble lids to create a code update image tar ball (#14)
    The firmware image for an inband code update is made up of
    individual binary files called LID files. As per current design,
    PLDM reassembles the individual LID files into a tarball image
    that can then be copied to the /tmp/images/ directory to trigger
    the same code update path as the Redfish API.
    
    Moving the Assemble lids part to phosphor-bmc-code-mgmt repo
    where the phosphor-bmc-code-mgmt code executes the shell script to
    assemble the lids and provide the result through activation
    interface (active/invalid) under the version path.
    
    Tested:
        Inband code update success and fail path.
    
    busctl tree xyz.openbmc_project.Software.BMC.Updater
    `-/xyz
      `-/xyz/openbmc_project
        `-/xyz/openbmc_project/software
          |-/xyz/openbmc_project/software/ac4336da
          `-/xyz/openbmc_project/software/e1c899ef
    
    In failure Case:
    Activation:
        busctl get-property xyz.openbmc_project.Software.BMC.Updater
           /xyz/openbmc_project/software/ac4336da
           xyz.openbmc_project.Software.Activation Activation
        --> s "xyz.openbmc_project.Software.Activation.Activations.Invalid"
    
    Version:
        busctl get-property xyz.openbmc_project.Software.BMC.Updater
           /xyz/openbmc_project/software/ac4336da
           xyz.openbmc_project.Software.Version Version
        --> s "null"
    
    In Success case:
    Activation:
        busctl get-property xyz.openbmc_project.Software.BMC.Updater
           /xyz/openbmc_project/software/e1c899ef
           xyz.openbmc_project.Software.Activation Activation
        --> s "xyz.openbmc_project.Software.Activation.Activations.Active"
    
    Version:
        busctl get-property xyz.openbmc_project.Software.BMC.Updater
           /xyz/openbmc_project/software/e1c899ef
           xyz.openbmc_project.Software.Version Version
        --> s "fw1030.20-19-1030.2315.20230404a (NL1030_055)"
    
    Signed-off-by: Archana Kakani <archana.kakani@ibm.com>

--- lid.hpp
+++ lid.hpp
@@ -3,8 +3,9 @@
 #include <sdbusplus/server.hpp>
 #include <xyz/openbmc_project/Software/LID/server.hpp>
 
+#include <chrono>
+#include <random>
 #include <string>
-#include <vector>
 
 namespace phosphor
 {
@@ -15,12 +16,21 @@ namespace manager
 
 using LidInherit = sdbusplus::server::object_t<
     sdbusplus::xyz::openbmc_project::Software::server::LID>;
+namespace sdbusRule = sdbusplus::bus::match::rules;
 
 class Lid : public LidInherit
 {
 
   public:
-    Lid(sdbusplus::bus_t& bus, const char* path) : LidInherit(bus, path)
+    Lid(sdbusplus::bus_t& bus, const char* path) :
+        LidInherit(bus, path), bus(bus),
+        systemdSignals(
+            bus,
+            sdbusRule::type::signal() + sdbusRule::member("JobRemoved") +
+                sdbusRule::path("/org/freedesktop/systemd1") +
+                sdbusRule::interface("org.freedesktop.systemd1.Manager"),
+            std::bind(std::mem_fn(&Lid::unitStateChange), this,
+                      std::placeholders::_1))
     {}
 
     /**
@@ -28,6 +38,39 @@ class Lid : public LidInherit
      *
      */
     void validate(std::string filePath);
+    /**
+     * @brief Create a image tar ball using lid files provided by host during
+     * inband code update
+     */
+    void assembleCodeUpdateImage();
+
+  private:
+    sdbusplus::bus_t& bus;
+    /** @brief Used to subscribe to dbus systemd signals **/
+    sdbusplus::bus::match_t systemdSignals;
+
+    /**
+     * @brief Check if systemd state change is relevant to this object
+     * Instance specific interface to handle the detected systemd state
+     * change
+     *
+     * @param[in]  msg       - Data associated with subscribed signal
+     *
+     */
+    void unitStateChange(sdbusplus::message_t& msg);
+
+    /** @brief Generate a random number
+     */
+    std::mt19937 randomGen{static_cast<unsigned>(
+        std::chrono::system_clock::now().time_since_epoch().count())};
+
+    /** @brief Create a version interface under /xyz/openbmc_project/software
+     *
+     * Creation of an empty version interface triggers the item updater to
+     * create an Invalid Activation object. Invalid activation is considered as
+     * failure during assembling lids.
+     */
+    void createVersionInterface();
 };
 
 } // namespace manager
