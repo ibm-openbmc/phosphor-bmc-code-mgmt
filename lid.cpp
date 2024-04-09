553ba403c68e587b3fbf9b16b3821bbb739b1d9a
Adriana Kobylak <anoo@us.ibm.com>
2024-04-09 13:47:41 -0500

    Marker LID Validation : Define validation dbus interface Backend (#17)
    This commit defines the dbus interface “validate” that will be
    called during inband code updates. As of now, the interface checks
    the validity of the access key. If the access key has expired and
    field mode is enabled, the dbus returns an error specifying the
    problem.
    
    This commit also adds restriction on HIPER service packs, being
    allowed across GA levels. HIPER service packs only within the same
    GA level will be allowed to be updated.
    Example: If a customer is on a build with the GA level of fw1030, they
    cannot move to the HIPER service pack released for fw1050.
    
    Previously, HIPER service packs could be applied across GA levels
    and this allowed customers to utilize the features of a latter service
    pack without buying the service contract.
    
    Tested:
    Used busctl call “busctl call xyz.openbmc_project.Software.BMC.Updater
    /xyz/openbmc_project/software xyz.openbmc_project.LID.Validate
    Validate s /media/hostfw/running-ro/80a00001.lid” to ensure that the
    call performs the validation call and the appropriate returns and
    errors are logged.
    
    Change-Id: I804e433be5d32e72d749c069b0c466b0493de4d6
    Signed-off-by: Sagar Srinivas <sagar.srinivas@ibm.com>

--- lid.cpp
+++ lid.cpp
@@ -5,9 +5,15 @@
 #include "utils.hpp"
 #include "version.hpp"
 
+#include <arpa/inet.h>
+
 #include <phosphor-logging/elog.hpp>
 #include <phosphor-logging/lg2.hpp>
+#include <xyz/openbmc_project/Software/Image/error.hpp>
 
+#include <cstring>
+#include <filesystem>
+#include <fstream>
 #include <string>
 
 #ifdef WANT_ACCESS_KEY_VERIFY
@@ -23,13 +29,114 @@ namespace manager
 namespace server = sdbusplus::server::xyz::openbmc_project::software;
 using VersionClass = phosphor::software::manager::Version;
 PHOSPHOR_LOG2_USING;
+using namespace phosphor::logging;
+using namespace sdbusplus::error::xyz::openbmc_project::software::image;
+using namespace phosphor::software::image;
+using VersionClass = phosphor::software::manager::Version;
 
 void Lid::validate(std::string filePath)
 {
 #ifdef WANT_ACCESS_KEY_VERIFY
+    struct markerLid
+    {
+        uint32_t versionId;
+        uint32_t offsetToMIKeywordSection;
+        uint32_t offsetToIseriesMarker;
+        uint32_t sizeOfMiKeyword;
+        char miKeyword[40];
+        char lastDisruptiveApplyFixLevel[3];
+        char lastDisruptiveActivationFSP[3];
+        char lastdisruptiveActivationPHYP[3];
+        char lastDisruptiveActivationPFW[3];
+        char lastChangedFixLevelForFSP[3];
+        char lastChangedFixLevelForPHYP[3];
+        char lastChangedFixLevelForPFW[3];
+        char reserved[3];
+        uint32_t offsetToAdditionalDataFields;
+    };
+    markerLid ml;
 
-    info("Update access key verification passed, {PATH}:", "PATH", filePath);
+    typedef struct
+    {
+        uint32_t SPFlagsOffset;
+        uint32_t SPFlagsSize;
+        uint32_t SPDateOffset;
+        uint32_t SPDateSize;
+    } markerFwIPAdfHeader_t;
 
+    typedef struct
+    {
+        uint32_t SPFlags;
+        char SPDate[9];
+    } fippAdfData_t;
+
+    // Open the Marker LID file and copy contents to the markerLid structure
+    std::ifstream efile(filePath, std::ios::in | std::ios::binary);
+    efile.read(reinterpret_cast<char*>(&ml), sizeof(ml));
+
+    using UpdateAccessKey = phosphor::software::image::UpdateAccessKey;
+    UpdateAccessKey updateAccessKey("");
+    uint32_t offBuff = htonl(ml.offsetToAdditionalDataFields);
+    uint32_t adfCount;
+    uint32_t adfSize;
+    uint32_t adfSignature;
+    markerFwIPAdfHeader_t markerFippAdfHeader;
+    std::string gaDate{};
+    std::string version{};
+
+    // Read the total number of ADFs present in the Marker LID file
+    efile.seekg(offBuff, efile.beg);
+    efile.read(reinterpret_cast<char*>(&adfCount), sizeof(adfCount));
+
+    // Loop through the ADFs to find respective ADF
+    for (uint8_t i = 0; i < htonl(adfCount); i++)
+    {
+        // Read the Size of ADF
+        efile.read(reinterpret_cast<char*>(&adfSize), sizeof(adfSize));
+
+        // Read the ADF signature Eg: FIPP, DSPNM, I5FX, HMCV, SECV etc
+        efile.read(reinterpret_cast<char*>(&adfSignature),
+                   sizeof(adfSignature));
+
+        // If the ADF is FIPP aka the firmware IP ADF
+        if (htonl(adfSignature) == markerAdfFippSig)
+        {
+            fippAdfData_t fippData;
+
+            // Read the ADF header into markerFippAdfHeader structure
+            efile.read(reinterpret_cast<char*>(&markerFippAdfHeader),
+                       sizeof(markerFippAdfHeader));
+
+            // Reading SP flag which indicates weather the service pack is an
+            // emergency service pack or one off service
+            efile.read(reinterpret_cast<char*>(&fippData.SPFlags),
+                       sizeof(fippData.SPFlags));
+
+            // Reading the GA date or the date when the service pack was
+            // released
+            efile.read(fippData.SPDate, sizeof(fippData.SPDate));
+
+            if (htonl(fippData.SPFlags) == hiperSPFlag)
+            {
+                isHiper = true;
+            }
+
+            gaDate = fippData.SPDate;
+
+            version = std::string(&ml.miKeyword[2], &ml.miKeyword[6]);
+            // Calling the UAK verify method
+            updateAccessKey.verify(gaDate, version, isHiper);
+            isHiper = false;
+            break;
+        }
+        else
+        {
+            // seek to the next ADF in the Marker LID
+            uint32_t nextADFOff = htonl(adfSize) - sizeof(adfSize) -
+                                  sizeof(adfSignature);
+            efile.seekg(nextADFOff, efile.cur);
+        }
+    }
 #endif
     return;
 }
