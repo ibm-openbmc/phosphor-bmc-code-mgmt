7d064f3da989ad9246c9943fca5bf0e04c14013a
Adriana Kobylak <anoo@us.ibm.com>
2024-07-15 11:58:34 -0500

    msl: Add sync method
    Upon start, the software updater service reads the Minimum Ship Level
    from the com.ibm.ipzvpd.VSYS FV property on the bmc card and from
    flash memory in /var/lib/phosphor-bmc-code-mgmt/msl-data.
    Then it syncs them up with one another, prioritizing first the bmc card,
    then flash memory.
    
    The sync mechanism is needed to keep a backup of the value in order to
    restore it if the planar or BMC card are replaced.
    
    Tested: Verified the syncing is done upon reboot. Verified no syncing is
    done when the value is all spaces (default), and when the VPD service is
    not running.
    
    Change-Id: I996f373711ac958a98e01fd65b09203016cbd69b
    Signed-off-by: Adriana Kobylak <anoo@us.ibm.com>

--- item_updater.cpp
+++ item_updater.cpp
@@ -283,6 +283,7 @@ void ItemUpdater::processBMCImage()
 
                 if (minimum_ship_level::enabled())
                 {
+                    minimum_ship_level::sync();
                     minimumVersionObject =
                         std::make_unique<MinimumVersion>(bus, path);
                     minimumVersionObject->minimumVersion(
